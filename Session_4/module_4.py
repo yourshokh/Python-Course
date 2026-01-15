23e1767135d35f37db527802e4459dae462c9b58
Shohujakhon Zikirov <157240767+yourshokh@users.noreply.github.com>
2026-01-15 09:05:22 -0600

    session 4 all tasks - initial  upload
    Implemented methods for tracking lecture attendance and homework completion.

--- Session_4/module_4.py
+++ Session_4/module_4.py
@@ -3,28 +3,50 @@ PASSING_GRADE = 8
 
 class Trainee:
     def __init__(self, name, surname):
-        pass
+        self.name = name
+        self.surname = surname
+
+        self.visited_lectures = 0
+        self.missed_lectures = 0
+        self.done_home_tasks = 0
+        self.missed_home_tasks = 0
+
+        self.mark = 0
 
     def visit_lecture(self):
-        pass
+        self.visited_lectures += 1
+        self._add_points(1)
 
     def do_homework(self):
-        pass
+        self.done_home_tasks += 2
+        self._add_points(2)
 
     def miss_lecture(self):
-        pass
+        self.missed_lectures -= 1
+        self._subtract_points(1)
 
     def miss_homework(self):
-        pass
+        self.missed_home_tasks -= 2
+        self._subtract_points(2)
 
     def _add_points(self, points: int):
-        pass
+        self.mark += points
+        if self.mark > 10:
+            self.mark = 10
 
     def _subtract_points(self, points):
-        pass
+        self.mark -= points
+        if self.mark < 0:
+            self.mark = 0
 
     def is_passed(self):
-        pass
+        if self.mark >= PASSING_GRADE:
+            print("Good job!")
+        else:
+            missing = PASSING_GRADE - self.mark
+            print(
+                f"You need to get {missing} more points. Try to do your best!"
+            )
 
     def __str__(self):
         status = (
