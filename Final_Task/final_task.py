1579af65faffb8509634336492b84c5f783e7ab0
Shohujakhon Zikirov <157240767+yourshokh@users.noreply.github.com>
2026-01-21 16:43:37 +0500

    Final task done

--- Final_Task/final_task.py
+++ Final_Task/final_task.py
@@ -3,18 +3,24 @@ Module for preparing inverted indexes based on uploaded documents
 """
 
 import sys
+import json
+import re
 from argparse import ArgumentParser, ArgumentTypeError, FileType
 from io import TextIOWrapper
-from typing import Dict, List
+from typing import Dict, List, Set
+from collections import defaultdict
 
 DEFAULT_PATH_TO_STORE_INVERTED_INDEX = "inverted.index"
 
+STOP_WORDS = {
+    'a', 'and', 'around', 'every', 'for', 'from', 'in', 'is', 'it', 
+    'not', 'on', 'one', 'the', 'to', 'under'
+}
 
 class EncodedFileType(FileType):
     """File encoder"""
 
     def __call__(self, string):
-        # the special argument "-" means sys.std{in,out}
         if string == "-":
             if "r" in self._mode:
                 stdin = TextIOWrapper(sys.stdin.buffer, encoding=self._encoding)
@@ -25,7 +31,6 @@ class EncodedFileType(FileType):
             msg = 'argument "-" with mode %r' % self._mode
             raise ValueError(msg)
 
-        # all other arguments are used as file names
         try:
             return open(string, self._mode, self._bufsize, self._encoding, self._errors)
         except OSError as exception:
@@ -44,11 +49,27 @@ class InvertedIndex:
     """
 
     def __init__(self, words_ids: Dict[str, List[int]]):
-        pass
+        self.index = words_ids
 
     def query(self, words: List[str]) -> List[int]:
         """Return the list of relevant documents for the given query"""
-        pass
+        if not words:
+            return []
+        
+
+        first_word = words[0]
+        if first_word not in self.index:
+            return []
+            
+        result_docs = set(self.index[first_word])
+        
+        for word in words[1:]:
+            if word in self.index:
+                result_docs.intersection_update(self.index[word])
+            else:
+                return []
+                
+        return list(result_docs)
 
     def dump(self, filepath: str) -> None:
         """
@@ -56,7 +77,8 @@ class InvertedIndex:
         :param filepath: path to file with documents
         :return: None
         """
-        pass
+        with open(filepath, 'w', encoding='utf-8') as file:
+            json.dump(self.index, file)
 
     @classmethod
     def load(cls, filepath: str):
@@ -65,7 +87,9 @@ class InvertedIndex:
         :param filepath: path to file with documents
         :return: InvertedIndex
         """
-        pass
+        with open(filepath, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return cls(data)
 
 
 def load_documents(filepath: str) -> Dict[int, str]:
@@ -74,7 +98,19 @@ def load_documents(filepath: str) -> Dict[int, str]:
     :param filepath: path to file with documents
     :return: Dict[int, str]
     """
-    pass
+    docs = {}
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            
+            try:
+                doc_id_str, content = line.lower().split("\t", 1)
+                docs[int(doc_id_str)] = content
+            except ValueError:
+                continue 
+    return docs
 
 
 def build_inverted_index(documents: Dict[int, str]) -> InvertedIndex:
@@ -83,7 +119,18 @@ def build_inverted_index(documents: Dict[int, str]) -> InvertedIndex:
     :param documents: dict with documents
     :return: InvertedIndex class
     """
-    pass
+    temp_index: Dict[str, Set[int]] = defaultdict(set)
+    
+    for doc_id, content in documents.items():
+        words = re.split(r"\W+", content)
+        
+        for word in words:
+            word = word.strip()
+            if word and word not in STOP_WORDS:
+                temp_index[word].add(doc_id)
+    
+    final_index = {word: list(doc_ids) for word, doc_ids in temp_index.items()}
+    return InvertedIndex(final_index)
 
 
 def callback_build(arguments) -> None:
@@ -116,13 +163,16 @@ def process_query(queries, index) -> None:
     :return: None
     """
     inverted_index = InvertedIndex.load(index)
+    
     for query in queries:
-        print(query[0])
         if isinstance(query, str):
-            query = query.strip().split()
-
-        doc_indexes = ",".join(str(value) for value in inverted_index.query(query))
-        print(doc_indexes)
+            query = query.strip().lower().split()
+        
+        if not query:
+            continue
+            
+        doc_indexes = inverted_index.query(query)
+        print(",".join(str(value) for value in doc_indexes))
 
 
 def setup_subparsers(parser) -> None:
@@ -172,7 +222,6 @@ def setup_subparsers(parser) -> None:
         "--query_from_file",
         dest="query",
         type=EncodedFileType("r", encoding="utf-8"),
-        # default=TextIOWrapper(sys.stdin.buffer, encoding='utf-8'),
         help="query file to get queries for inverted index",
     )
     query_parser.set_defaults(callback=callback_query)
